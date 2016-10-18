3882ac257d2d16049d4ee686a6b4d29d1dae23d8
blackstone86 <jimvin@hudongpai.com>
2016-10-18 22:57:14 +0800

    [practice/practice.js] 新增引用foo模块

--- components/pages/practice/practice.js
+++ components/pages/practice/practice.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// 引用其他模块
+var foo = require('pages/foo');
+// 引用当前目录下的文件
+// var conf = require('./conf.js');
+var conf = require('practice-conf');
+
 exports.getMarkdown = function(){
-    return __inline('doc.md');
+    var fooHtml = foo.getHTML(); 
+    var config = conf.getConf();
+    console.log(config);
+    return __inline('doc.md') + fooHtml;
 };
