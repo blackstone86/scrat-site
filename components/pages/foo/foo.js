6df054cd606838427196a6da12b68e7f44c18bef
blackstone86 <jimvin@hudongpai.com>
2016-10-18 22:54:09 +0800

    [foo/foo.js] 初始化foo.js

---
+++ components/pages/foo/foo.js
@@ -0,0 +1,14 @@
+// 使用__inline函数嵌入其他资源，如果
+// 是图片，构建后会替换为base64字符串
+var tpl = __inline('foo.tpl');
+var iconBase64 = __inline('imgs/favicon.ico');
+// 使用__uri函数定位资源，构建后会替换
+// 为部署后的绝对路径
+var icon = __uri('imgs/favicon.ico');   
+
+//像nodejs那样导出API
+exports.getHTML = function(){
+    console.log(icon);
+    console.log(iconBase64);
+    return tpl;
+};
