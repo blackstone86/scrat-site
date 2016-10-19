4a37563549bf50e4aaae18df8c4d228becf5525d
jimvin <jimvin@hudongpai.com>
2016-10-19 13:24:26 +0800

    [fis-conf.js] 增加配置说明

--- fis-conf.js
+++ fis-conf.js
@@ -1,24 +1,43 @@
-// 排除源码目录下的node_modules目录，不对其进行构建
-fis.config.set('project.exclude', 'node_modules/**');
-
 // 利用package.json文件定义项目名和版本
 var meta = require('./package.json');
+
+/**
+ * scrat version v0.8.0
+ * name            必须设置
+ * version         必须设置
+ * comboPattern    建议设置，便于区分哪些是合并请求 默认值 /??%s，建议/co??%s 或 /combo??%s
+ * cache           按需设置，建议生产环境开启，即使scrat release -o开启，也是按配置来执行
+ * alias           按需设置，设置非主文件模块，包括js，css等
+ * combo           不设置，只能通过scrat release -p开启
+ * urlPattern      一般不设置，默认值 /c/%s
+ * hash            不设置，scrat release 自动计算
+ * deps            不设置，scrat release 自动计算
+ * project.exclude 建议设置，一般不将源码node_modules迁移到发布目录
+ * urlPrefix       与framework.urlPattern搭配使用
+ */
+// 项目名称
 fis.config.set('name', meta.name);
+// 项目版本号
 fis.config.set('version', meta.version);
+// scrat.js框架开启localstorage缓存
+fis.config.set('framework.cache', false);
+// 静态资源加载路径模式
+fis.config.set('framework.urlPattern', '/public/c/%s');
+// 设置url前缀
+fis.config.set('urlPrefix', '/public');
+// 定义别名 
+fis.config.set('framework.alias.practice-conf', 'components/pages/practice/conf.js');
+fis.config.set('framework.alias.foo_plus', 'components/pages/foo/foo_plus.css');
+// 设置combo
+fis.config.set('framework.combo', true);
+// 指定combo的url格式
+fis.config.set('framework.comboPattern', '/combo??%s');
+// 排除源码目录下的node_modules目录，不对其进行构建 构建指将源码迁移到发布目录下
+fis.config.set('project.exclude', ['node_modules/**']);
 
 // 对md、tpl后缀的文件指定用fis-optimizer-html-minifier插件进行压缩
 fis.config.set('modules.optimizer.md', 'html-minifier');
 fis.config.set('modules.optimizer.tpl', 'html-minifier');
-
-// scrat.js框架开启localstorage缓存
-fis.config.set('framework.cache', true);
-// 静态资源加载路径模式
-fis.config.set('framework.urlPattern', '/public/c/%s');
-// 定义别名 
-fis.config.set('framework.alias.practice-conf', 'components/pages/practice/conf.js');
-
-//设置url前缀
-fis.config.set('urlPrefix', '/public');
 
 // fis-lint-jshint插件配置
 fis.config.set('settings.lint.jshint', {
